984efcf55e19e75727c0e7962fa88ddb99189501
Quentin RODRIGUES <quentin.rodrigues1912@gmail.com>
2024-05-31 10:30:09 +0200

    feat: modification et affichage d'un blog

--- front/src/components/Blogs/ShowBlog.vue
+++ front/src/components/Blogs/ShowBlog.vue
@@ -1,48 +1,50 @@
 <template>
-    <v-divider></v-divider>
-    <h3 class="mt-2">Bienvenue sur le blog {{ blog.title }}</h3>
-    <h4 v-if="blog.articles.length !== 0">Consultez {{ blog.articles.length <= 1 ? "son unique article" : `ses ${blog.articles.length} articles` }} ci-dessous :</h4>
-    <h4 v-else>Le blog n'a pas encore d'article !</h4>
-    <v-card flat>
-        <template v-slot:text>
-            <v-text-field v-model="search" label="Rechercher un article..." prepend-inner-icon="mdi-magnify"
-                variant="outlined" hide-details single-line></v-text-field>
-        </template>
+    <div v-if="!loading">
+        <v-divider></v-divider>
+        <h3 class="mt-2">Bienvenue sur le blog {{ blog.title }}</h3>
+        <h4 v-if="blog.articles.length !== 0">Consultez {{ blog.articles.length <= 1 ? "son unique article" : `ses
+                ${blog.articles.length} articles` }} ci-dessous :</h4>
+                <h4 v-else>Le blog n'a pas encore d'article !</h4>
+                <v-card flat>
+                    <template v-slot:text>
+                        <v-text-field v-model="search" label="Rechercher un article..." prepend-inner-icon="mdi-magnify"
+                            variant="outlined" hide-details single-line></v-text-field>
+                    </template>
 
-        <v-data-table :headers="headers" :items="articles" :search="search">
-            <template v-slot:item.actions="{ item }">
-                <ActionsMenu @click:item="onClickItem($event, item.id)" :items="actions" />
-            </template>
-        </v-data-table>
-    </v-card>
-    <v-dialog v-model="deleteDialog" width="auto">
-        <v-card max-width="400" prepend-icon="mdi-delete" text="Êtes-vous sûr de vouloir supprimer cet article ?"
-            title="Suppression de blog">
-            <div class="d-flex justify-end pa-1">
-                <v-btn color="info" text="Annuler" @click="deleteDialog = false"></v-btn>
-                <v-btn class="ms-1" color="error" text="Supprimer" @click="deleteDialog = false; onDeleteArticle()"></v-btn>
-            </div>
-        </v-card>
-    </v-dialog>
+                    <v-data-table :headers="headers" :items="blog.articles" :search="search">
+                        <template v-slot:item.actions="{ item }">
+                            <ActionsMenu @click:item="onClickItem($event, item.id)" :items="actions" />
+                        </template>
+                    </v-data-table>
+                </v-card>
+                <v-dialog v-model="deleteDialog" width="auto">
+                    <v-card max-width="400" prepend-icon="mdi-delete"
+                        text="Êtes-vous sûr de vouloir supprimer cet article ?" title="Suppression de blog">
+                        <div class="d-flex justify-end pa-1">
+                            <v-btn color="info" text="Annuler" @click="deleteDialog = false"></v-btn>
+                            <v-btn class="ms-1" color="error" text="Supprimer"
+                                @click="deleteDialog = false; onDeleteArticle()"></v-btn>
+                        </div>
+                    </v-card>
+                </v-dialog>
+    </div>
 </template>
 <script setup>
-import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-
-const router = useRouter()
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { getBlog } from '@/repository/blogs';
 
 const route = useRoute()
 const id = route.params.id
 
-const blog = {
-    title: 'InformatiK',
-    articles: ['a', 'b']
-}
+const blog = ref()
+const loading = ref(true)
 
 const deleteDialog = ref()
 const selectedItem = ref()
 
 const search = ref('')
+
 const headers = [
     {
         align: 'start',
@@ -53,22 +55,7 @@ const headers = [
     { key: 'ranking', title: 'Notation' },
     { key: 'actions', title: 'Actions', align: 'end' }
 ]
-const articles = [
-    {
-        id: 1,
-        title: 'Informatik',
-        topic: "Tous les sujets sur l'informatique.",
-        date: '30/05/2024',
-        articles_quantity: 7
-    },
-    {
-        id: 2,
-        title: 'Informatik',
-        topic: "Tous les sujets sur l'informatique.",
-        date: '30/05/2024',
-        articles_quantity: 7
-    }
-]
+
 const actions = [
     {
         title: 'Supprimer',
@@ -77,7 +64,7 @@ const actions = [
 ]
 
 // Methods
-function onClickItem (action, item) {
+function onClickItem(action, item) {
     selectedItem.value = item
     switch (action) {
         case 'delete':
@@ -89,4 +76,10 @@ function onClickItem (action, item) {
 function onDeleteArticle() {
     alert('Suppression de ' + selectedItem.value)
 }
+
+// Lifecycle
+onMounted(async () => {
+    blog.value = await getBlog(id)
+    loading.value = false
+})
 </script>
--- front/src/repository/blogs.js
+++ front/src/repository/blogs.js
@@ -6,16 +6,41 @@ export async function getBlogs () {
     return res.data
 }
 
-export function getBlog(id) {
-    axios.get(url + 'blogs/' + id).then((res, req) => {
-        return res.data
-    })
+export async function getBlog(id) {
+    const res = await axios.get(url + 'blogs/' + id)
+    return res.data
 }
 
 export async function deleteBlog(id) {
     try {
         await axios.delete(url + 'blogs/' + id)
         return true
+    } catch {
+        return false
+    }
+}
+
+export async function createBlog(title, topic, date) {
+    try {
+        const res = await axios.post(url + 'blogs', {
+            title,
+            topic,
+            date
+        })
+        return res
+    } catch {
+        return false
+    }
+}
+
+export async function updateBlog(id, title, topic, date) {
+    try {
+        const res = await axios.patch(url + 'blogs/' + id, {
+            title,
+            topic,
+            date
+        })
+        return res
     } catch {
         return false
     }
